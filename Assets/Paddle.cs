0a39bd8a007d184abc117f60d23f025ff3ca6511
Jeremy Weeks <crono9997@gmail.com>
2016-08-03 09:10:24 -0700

    Initial Commit

---
+++ Assets/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 v3 = Input.mousePosition;
+		v3 = Camera.main.ScreenToWorldPoint(v3);
+
+		v3.z = 0;
+		v3.y = transform.position.y;
+		v3.x = Mathf.Clamp(v3.x, -6.9f, 6.9f);
+		transform.position = v3;
+	}
+}
